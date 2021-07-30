07856e4e04c7febd97deb8e5452aac941db23afe
“yuyangyyu” <2771342603@qq.com>
2021-07-30 15:30:03 +0100

    chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
+# Register your models here.
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
+# Add in this class to customise the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Page,Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
@@ -22,15 +23,38 @@ class PageForm(forms.ModelForm):
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
+        # Provide an association between the ModelForm and a model
         model = Page
+
+        # What fields do we want to include in our form?
+        # This way we don't need every field in the model present.
+        # Some fields may allow NULL values; we may not want to include them.
+        # Here, we are hiding the foreign key.
+        # we can either exclude the category field from the form,
         exclude = ('category',)
+        # or specify the fields to include (don't include the category field).
+        # fields = ('title', 'url', 'views')
 
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
         if url and not url.startswith('http://'):
             url = f'http://{url}'
             cleaned_data['url'] = url
 
         return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
